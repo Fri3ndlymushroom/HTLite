332cb62043f7f791d62e528f102f9915c506e3e1
Moritz <morihues0@gmail.com>
2022-07-02 02:00:50 +0200

    before back migration to google

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
     "function": {
-        "userHandler": {
+        "gettest": {
             "Name": "string",
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
-        }
-    },
-    "api": {
-        "apic896f31c": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    },
-    "auth": {
-        "htlite0f182147": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
         }
     }
 }
--- src/pages/Authentication/Authentication.tsx
+++ src/pages/Authentication/Authentication.tsx
@@ -12,10 +12,24 @@ export default function Authentication({Auth}: any) {
 const [status, setStatus] = useState({color: "green", message: ""})
 
     const SignUp = () =>{
+        console.log(email, password, nickname)
         Auth.signUp({
             username: email,
             password: password,
             attributes: { nickname: nickname }
+        }).then((response:any) => {
+            setStatus({ color: "green", message: "you have been registered" })
+        }).catch((err:any) => {
+            console.error(err)
+            setStatus({ color: "red", message: err.message })
+        })
+    }
+
+    const signUpWithTest = () =>{
+        Auth.signUp({
+            username: "test@test.test",
+            password: "testpassword",
+            attributes: { nickname: "testuser" }
         }).then((response:any) => {
             setStatus({ color: "green", message: "you have been registered" })
         }).catch((err:any) => {
@@ -28,16 +42,17 @@ const [status, setStatus] = useState({color: "green", message: ""})
             
             <div>
                 <p>Sign Up</p>
-                <input onChange={(text:any)=>{setEmail(text)}} placeholder="Email"></input>
-                <input onChange={(text:any)=>{setPassword(text)}} placeholder="Password"></input>
-                <input onChange={(text:any)=>{setNickname(text)}} placeholder="Nickname"></input>
+                <input onChange={(text:any)=>{setEmail(text.target.value)}} placeholder="Email"></input>
+                <input onChange={(text:any)=>{setPassword(text.target.value)}} placeholder="Password"></input>
+                <input onChange={(text:any)=>{setNickname(text.target.value)}} placeholder="Nickname"></input>
                 <button onClick={() => { SignUp()}}>sign up</button>
             </div>
             <div>
                 <p>Sign In</p>
-                <input onChange={(text:any)=>{setEmail(text)}} placeholder="Nickname"></input>
-                <input onChange={(text:any)=>{setPassword(text)}} placeholder="Password"></input>
+                <input onChange={(text:any)=>{setEmail(text.target.value)}} placeholder="Nickname"></input>
+                <input onChange={(text:any)=>{setPassword(text.target.value)}} placeholder="Password"></input>
                 <button onClick={() => { }}>sign up</button>
+                <button onClick={()=>signUpWithTest()}>signin with test account</button>
             </div>
             <p style={{color: status.color}}>{status.message}</p>
 
